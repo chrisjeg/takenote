5ccf42ab56d7674f92e9a80d69e7ed974d80276e
Chris Jeganathan <chris.jeganathan@gmail.com>
2017-02-02 23:57:16 +0000

    Added colourisation code

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var midiInput = new midi.input();
 midiInput.openPort(1);
 
 var note = new TakeNote({midiInput});
-setTimeout(()=>console.log(note.map),1000);
-setTimeout(()=>console.log(note.chromaMap),1000);
 
 process.on('SIGINT', function () {
   midiInput.closePort();
--- takenote.js
+++ takenote.js
@@ -2,7 +2,15 @@ const EventEmitter = require('events');
 const helper = require('./lib/midiHelpers');
 
 class TakeNote extends EventEmitter{
-	constructor({midiInput, lowNote = 0, highNote = 120}){
+	constructor({
+		midiInput,
+		lowNote = 0,
+		highNote = 120,
+		firstLedNote,
+		lastLedNote,
+		numLeds = 30,
+		isInverted = false
+	}){
 		super();
 		this.lowNote = lowNote;
 		this.firstLedNote = firstLedNote;
@@ -10,29 +18,39 @@ class TakeNote extends EventEmitter{
 		this.lastLedNote = lastLedNote;
 		this.numLeds = numLeds;
 		this.isInverted = isInverted;
+		this.colours = 12;
 
 		this.keyboardMap = Array(highNote-lowNote).fill(0).map(()=>({v:0,chords:[]}));
-		console.log(this.keyboardMap[91]);
 		this.activeChords = {};
 
-		var input = midiInput;
-		input.on('message',(delta,message)=>{
+		midiInput.on('message',(delta,message)=>{
 			let event = helper.toEvent(message);
 			if(event.id === 'key'){
 				this.keyboardMap[event.key].v = event.velocity;
 			}
-			console.log(event);
 		});
 
+		//Sets the LEDs to die, even if a key off hasn't been registered
 		setInterval(()=>{
-			this.keyboardMap.forEach((key,index,map)=>{	
+			this.keyboardMap.forEach((key,index,map)=>{
 				if(map[index].v > 0) map[index].v -= 1;
 			});
 		},15);
+
 	}
 
 	chordCalculator(){
 
+	}
+
+	colouriseKey(key,velocity){
+		let r,g,b,v;
+		v = velocity*2; //velocity is half of brightness
+		let n = helper.normalise(key);
+		r = helper.calculateBrightness(n,0)*v;
+		g = helper.calculateBrightness(n,(2/3))*v;
+		b = helper.calculateBrightness(n,(1/3))*v;
+		return helper.toBinaryRGB(r,g,b);
 	}
 
 	get map(){
